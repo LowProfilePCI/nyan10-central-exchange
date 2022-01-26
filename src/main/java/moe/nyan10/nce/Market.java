29abbd3cac18c8fcf7811d8b9b37d405d607ef1d
LowProfilePCI <lowprofilepci@gmail.com>
2022-01-27 05:32:47 +0900

    first commit

---
+++ src/main/java/moe/nyan10/nce/Market.java
@@ -0,0 +1,27 @@
+package moe.nyan10.nce;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Market extends JavaPlugin {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		return true;
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onEnable();
+	}
+
+}
